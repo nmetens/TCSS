978a8f11f2523de5a8959ed2f450285256a48ee1
Nathan Metens <nathanmetens1@gmail.com>
2025-11-07 13:11:52 -0800

    Animal huerarchy with dynamic binding examples for lab 6

---
+++ CS302/dynamic_binding_example/dynamic.cpp
@@ -0,0 +1,42 @@
+#include "dynamic.h"
+#include <iostream>
+using namespace std;
+
+Animal::Animal(int weight, string noise) {
+    this->weight = weight;
+    this->noise = noise;
+}
+
+void Animal::display() {
+   cout << "Weight: " << weight << endl;
+   cout << "Noise: " << noise << endl;
+}
+
+void Animal::sound() {
+    cout << noise << endl;
+}
+
+//#############################
+
+Dog::Dog(int weight, string noise, int biteforce): Animal(weight, noise), biteforce(biteforce) {}
+
+void Dog::display() {
+    cout << "Biteforce: " << biteforce << endl;
+}
+
+void Dog::sound() {
+    cout << noise << endl;
+}
+
+//#############################
+
+Cat::Cat(int weight, string noise, float jump_height): Animal(weight, noise), jump_height(jump_height) {}
+
+void Cat::display() {
+    cout << "Jump height: " << jump_height << endl;
+}
+
+void Cat::sound() {
+    cout << noise << endl;
+}
+
